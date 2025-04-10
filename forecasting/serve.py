9c772a94d1c429b2386dc0566ef4f24c32e6deea
Mahone <87675992+ID-Mahone@users.noreply.github.com>
2025-04-11 00:20:25 +0200

    added logging

--- forecasting/serve.py
+++ forecasting/serve.py
@@ -6,6 +6,10 @@ from prophet import Prophet
 import os
 import pandas as pd
 import joblib
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 
@@ -28,25 +32,35 @@ def read_root():
 @app.get("/predict")
 def get_forecast(days: int = Query(default=30, ge=1, le=365)):
     if not model:
+        logger.error("Model not loaded!")
         return JSONResponse(content={"error": "Model not loaded"}, status_code=500)
 
     try:
+        logger.info(f"Generating forecast for {days} days")
         future = model.make_future_dataframe(periods=days)
+        logger.debug(f"Future DataFrame:\n{future.head()}")
+
         forecast = model.predict(future)
         forecast_subset = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(days)
 
         #Save forecast data to DB 
+        logger.info(f"Saving forecast data to DB...")
         for index, row in forecast_subset.iterrows():
             db.add(forecast(ds=row['ds'], yhat=row['yhat'], yhat_lower=row['yhat_lower'], yhat_upper=row['yhat_upper']))
 
         db.commit()
+        logger.info("Forecast data saved successfully")
 
         forecast_json = forecast_subset.to_dict(orient="records")
         return forecast_json
+
     except SQLAlchemyError as e:
         db.rollback()
+        logger.error(f"SQLAlchemy error: {str(e)}")
         return JSONResponse(content={"error": str(e)}, status_code=500)
+
     except Exception as e:
+        logger.error(f"Unexpected error: {strg(e)}")
         return JSONResponse(content={"error": str(e)}, status_code=500)
 
 @app.get("/get_forecasts")
